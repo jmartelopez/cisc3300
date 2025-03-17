f6a7cd53a2236a25c1f9bc0a9d20dd7c8438b294
Jasmin Marte Lopez <jmartelopez@fordham.edu>
2025-03-16 21:14:43 -0400

    Homework/HW7/index.php

--- Homework/HW7/index.php
+++ Homework/HW7/index.php
@@ -1,6 +1,13 @@
 <?php
-require 'homework-7.php';
 
-$controller = new \controllers\NoteController();
-$controller->handleRequest();
+require './controller/NoteController.php';
+
+$noteController = new NoteController();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $noteController->saveNote();
+} else {
+    $noteController->viewAddNotes();
+}
 ?>
+
